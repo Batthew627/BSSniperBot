028be06f1da13d0d14c86f7df7a1c210cd5f1377
connor pearson <34844662+Liconnorli@users.noreply.github.com>
2023-04-24 21:52:32 +0100

    Sync playlist for me and august

--- src/index.ts
+++ src/index.ts
@@ -7,43 +7,9 @@ import path, { resolve } from 'path';
 import express from 'express';
 import ScoreSaberAPI from 'scoresaber.js';
 import { getPlayerData, rankedPlaylistByStarValue, snipePlaylist, writePlaylist } from 'bssniper';
-import cors from 'cors';
 
 config({ path: resolve(__dirname, '..', '.env') });
 import './client';
-
-
-const app = express();
-const port = 8080;
-app.use(cors());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.get('/index', async (_req, res) => {
-    const temp = await snipedata();
-    res.send(temp);
-});
-
-
-app.listen(port, () => console.log(`Server listening on port: ${port}`));
-
-app.use('/static', express.static('static'));
-
-app.post('/update-html', (req, res) => {
-    const htmlFilePath = path.join(__dirname, 'public', 'index.html');
-    const newHtmlContent = ' ';
-
-    fs.writeFile(htmlFilePath, newHtmlContent, (err: any) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Failed to update HTML content');
-        } else {
-            res.send('HTML content updated successfully');
-        }
-    });
-});
 
 export function windowsFileNamify(file: string) {
     return file.replace(/[\/\\:*?"<>|]/g, '_');
@@ -60,11 +26,15 @@ setInterval(async () => {
     }
 }, 60000);
 
-async function snipedata() {
-    const august = (await getPlayerData('76561199073044136'));
-    const batthew = (await getPlayerData('76561198121538359'));
-    const batSnipe = (await snipePlaylist(august, batthew, false)).songs.length;
-    const augSnipe = (await snipePlaylist(batthew, august, false)).songs.length;
-    const totalScores = batSnipe + augSnipe;
-    return `${batSnipe} / ${totalScores}`;
-}
+setInterval(async () =>{
+    const bat = await getPlayerData('76561198121538359');
+    const aug = await getPlayerData('76561199073044136');
+    const batPlaylist = await snipePlaylist(bat, aug, false);
+    const augPlaylist = await snipePlaylist(aug, bat, false);
+    batPlaylist.syncURL=`http://batthew.co.uk:8080/static/${batPlaylist.playlistTitle}`;
+    augPlaylist.syncURL=`http://batthew.co.uk:8080/static/${augPlaylist.playlistTitle}`;
+    await writePlaylist(batPlaylist, './static');
+    await writePlaylist(augPlaylist, './static');
+}, 300000);
+
+
